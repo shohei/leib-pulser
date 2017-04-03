2f4c7cf7bf410ec7b60248c41991ce936eebf4ed
Shohei Aoki <shoaok@gmail.com>
2017-04-03 14:16:06 +0900

    add test code

---
+++ simple_tb.v
@@ -0,0 +1,18 @@
+module simple_tb;
+
+   reg [3:0] A = 4'b1010;
+   wire [3:0] B;
+
+   initial
+     begin
+        $dumpfile("simple.vcd");
+        $dumpvars(0, s);
+        $monitor("A is %b, B is %b.", A, B);
+
+        #50 A = 4'b1100;
+        #100 A = 4'b0000;
+        #100 $finish;
+     end
+
+   simple s(A, B);
+endmodule
